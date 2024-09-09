49f028b1f855997a11baf5a3670ef21e569f057f
Shruti <chadwalshruti@gmail.com>
2024-09-09 18:07:31 +0530

    initial commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+    # cursor.execute('insert into Performance values (GETDATE(),'
+    #                + str(cpu_usage) + ','
+    #                + str(memory_usage) + ','
+    #                + str(cpu_interrupts) + ','
+    #                + str(cpu_calls) + ','
+    #                + str(memory_used) + ','
+    #                + str(memory_free) + ','
+    #                + str(bytes_sent) + ','
+    #                + str(bytes_received) + ','
+    #                + str(disk_usage) + ')'
+    #                )
+    # con.commit()
